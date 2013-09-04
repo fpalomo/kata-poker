5f89889c60877b5b64dcefb6703261b3ca966cb9
Fernando Palomo Garcia <fernando@bluejay.es>
2013-09-04 16:35:41 +0200

    add phpunit dev. test couple RED

--- Hand.php
+++ Hand.php
@@ -11,26 +11,18 @@ namespace Poker;
 class Hand
 {
 
-    const COUPLE = 2;
-    const DOUBLE_COUPLE = 3;
-    const TRIO = 4;
-    const LADDER = 5;
-    const COLOR = 6;
-    const FULL = 7;
+    const COUPLE=2;
 
     public $cards;
 
     public function __construct(array $cards)
     {
-        if (count($cards) != 5) {
+        if (count ($cards)!=5){
             throw new \exception ("wrong number of cards");
         }
         foreach ($cards as $card) {
             if (!$card instanceof Card) {
                 throw new \exception("that is not a card!");
-            }
-            if ($card->number < 1 || $card->number > 13) {
-                throw new \exception("the card number is not valid");
             }
         }
 
@@ -38,62 +30,8 @@ class Hand
     }
 
 
-    public function winnerCombination()
-    {
-        $numbers = array();
-        $suits = array();
-        $result = 0;
-        foreach ($this->cards as $card) {
-            array_push($numbers, $card->number);
-            array_push($suits, $card->suit);
-        }
-        $totalValues = array_count_values($numbers);
-        $totalSuites = array_count_values($suits);
-        // full
-        if (count($totalValues) == 2) {
-            $result = self::FULL;
-        }
-        if ($result < self::FULL) {
-            // color
-            if (count($totalSuites) == 1) {
-                $result = self::COLOR;
-            }
-
-            // ladder
-            if ($result < self::COLOR) {
-                sort($numbers);
-                $result = self::LADDER;
-                for ($i = 1; $i < count($numbers); $i++) {
-                    if ($numbers[$i] != $numbers[$i - 1] + 1) {
-                        $result = null;
-                        break;
-                    }
-                }
-                if ($result < self::LADDER) {
-                    foreach ($totalValues as $number) {
-                        // trio
-                        if ($number == 3) {
-                            if ($result < self::TRIO) {
-                                $result = self::TRIO;
-                            }
-                        }
-                        if ($number == 2) {
-                            // double couple
-                            if ($result == self::COUPLE) {
-                                $result = self::DOUBLE_COUPLE;
-                            } else {
-                                // couple
-                                if ($result < self::COUPLE) {
-                                    $result = self::COUPLE;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        return $result;
+    public function winnerCombination(){
+        return false;
     }
 
 
--- test/HandTest.php
+++ test/HandTest.php
@@ -36,7 +36,7 @@ class HandTest extends \PHPUnit_Framework_TestCase
      * //TODO specific exceptions per error
      * @expectedException exception
      */
-    public function testInvalidConstructWrongNumberofCards()
+    public function testInvalidConstructWrongNumber()
     {
         $cards = array(
             new Card(2, Card::DIAMONDS),
@@ -67,25 +67,6 @@ class HandTest extends \PHPUnit_Framework_TestCase
     }
 
 
-
-    /**
-     * //TODO specific exceptions per error
-     * @expectedException exception
-     */
-    public function testInvalidConstructWrongContentCardWithWrongNumber()
-    {
-        $cards = array(
-            new Card(112, 44),
-            new Card(2, Card::DIAMONDS),
-            new Card(4, Card::DIAMONDS),
-            new Card(2, Card::DIAMONDS),
-        );
-
-        $hand = new Hand($cards);
-
-    }
-
-
     public function testCouple()
     {
         $cards = array(
@@ -101,125 +82,6 @@ class HandTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(Hand::COUPLE, $hand->winnerCombination());
 
     }
-
-    public function testNoValidCombination()
-    {
-        $cards = array(
-            new Card(1, Card::CLUBS),
-            new Card(2, Card::HEARTS),
-            new Card(4, Card::DIAMONDS),
-            new Card(7, Card::DIAMONDS),
-            new Card(9, Card::DIAMONDS),
-        );
-
-        $hand = new Hand($cards);
-
-        $this->assertEquals(null, $hand->winnerCombination());
-
-    }
-
-
-    public function testDoubleCouples(){
-        $cards = array(
-            new Card(1, Card::CLUBS),
-            new Card(1, Card::HEARTS),
-            new Card(2, Card::DIAMONDS),
-            new Card(2, Card::PIKES),
-            new Card(9, Card::DIAMONDS),
-        );
-
-        $hand = new Hand($cards);
-
-        $this->assertEquals(Hand::DOUBLE_COUPLE, $hand->winnerCombination());
-
-    }
-
-
-
-    public function testTrioSimple(){
-        $cards = array(
-            new Card(1, Card::CLUBS),
-            new Card(1, Card::HEARTS),
-            new Card(1, Card::DIAMONDS),
-            new Card(2, Card::PIKES),
-            new Card(9, Card::DIAMONDS),
-        );
-
-        $hand = new Hand($cards);
-
-        $this->assertEquals(Hand::TRIO, $hand->winnerCombination());
-
-    }
-
-
-    public function testLadderSimple(){
-        $cards = array(
-            new Card(1, Card::CLUBS),
-            new Card(2, Card::HEARTS),
-            new Card(3, Card::DIAMONDS),
-            new Card(4, Card::PIKES),
-            new Card(5, Card::DIAMONDS),
-        );
-
-        $hand = new Hand($cards);
-
-        $this->assertEquals(Hand::LADDER, $hand->winnerCombination());
-
-    }
-
-
-
-    public function testColorSimple(){
-        $cards = array(
-            new Card(1, Card::CLUBS),
-            new Card(9, Card::CLUBS),
-            new Card(3, Card::CLUBS),
-            new Card(4, Card::CLUBS),
-            new Card(5, Card::CLUBS),
-        );
-
-        $hand = new Hand($cards);
-
-        $this->assertEquals(Hand::COLOR, $hand->winnerCombination());
-
-    }
-
-
-
-    public function testColorVsLadder(){
-        $cards = array(
-            new Card(1, Card::CLUBS),
-            new Card(2, Card::CLUBS),
-            new Card(3, Card::CLUBS),
-            new Card(4, Card::CLUBS),
-            new Card(5, Card::CLUBS),
-        );
-
-        $hand = new Hand($cards);
-
-        $this->assertEquals(Hand::COLOR, $hand->winnerCombination());
-
-    }
-
-
-
-    public function testFullVsDoubleCouples(){
-        $cards = array(
-            new Card(1, Card::CLUBS),
-            new Card(1, Card::DIAMONDS),
-            new Card(1, Card::PIKES),
-            new Card(5, Card::CLUBS),
-            new Card(5, Card::DIAMONDS),
-        );
-
-        $hand = new Hand($cards);
-
-        $this->assertEquals(Hand::FULL, $hand->winnerCombination());
-
-    }
-
-
-
 
 
 }
