b55e3897323baad590adba68e173f39c39811ce4
Fernando Palomo Garcia <fernando@bluejay.es>
2013-09-04 16:53:27 +0200

    color GREEN and REFACTOR

--- Hand.php
+++ Hand.php
@@ -40,39 +40,51 @@ class Hand
     public function winnerCombination()
     {
         $numbers = array();
+        $suits = array();
         $result = 0;
         foreach ($this->cards as $card) {
             array_push($numbers, $card->number);
+            array_push($suits, $card->suit);
         }
         $values = array_count_values($numbers);
-        // ladder
-        sort($numbers);
-        $result = self::LADDER;
-        for ($i = 1; $i < count($numbers); $i++) {
-            if ($numbers[$i] != $numbers[$i - 1] + 1) {
-                $result = null;
-                break;
-            }
+        $totalSuites = array_count_values($suits);
+        // color
+        if (count($totalSuites) == 1) {
+            $result = self::COLOR;
         }
-        foreach ($values as $number) {
-            // trio
-            if ($number == 3) {
-                if ($result < self::TRIO) {
-                    $result = self::TRIO;
+
+
+        // ladder
+        if ($result < self::COLOR) {
+            sort($numbers);
+            $result = self::LADDER;
+            for ($i = 1; $i < count($numbers); $i++) {
+                if ($numbers[$i] != $numbers[$i - 1] + 1) {
+                    $result = null;
+                    break;
                 }
             }
-            if ($number == 2) {
-                // double couple
-                if ($result == self::COUPLE) {
-                    $result = self::DOUBLE_COUPLE;
-                } else {
-                    // couple
-                    if ($result < self::COUPLE) {
-                        $result = self::COUPLE;
+            if ($result < self::LADDER) {
+                foreach ($values as $number) {
+                    // trio
+                    if ($number == 3) {
+                        if ($result < self::TRIO) {
+                            $result = self::TRIO;
+                        }
+                    }
+                    if ($number == 2) {
+                        // double couple
+                        if ($result == self::COUPLE) {
+                            $result = self::DOUBLE_COUPLE;
+                        } else {
+                            // couple
+                            if ($result < self::COUPLE) {
+                                $result = self::COUPLE;
+                            }
+                        }
                     }
                 }
             }
-
         }
 
         return $result;
